27a66e4ea282b4da6a01715dc3ef68ca523af8f8
summerine <indri.pr.26@gmail.com>
2018-12-16 17:11:42 +0700

    add alert, cari bar and tambah data button

--- tampil_data.php
+++ tampil_data.php
@@ -28,15 +28,70 @@ else {
 
  			elseif ($_GET['alert'] == 1) { ?>
  				<div class="alert alert-success alert-dismissable fade show" role="alert">
- 					<strong><i class="fas fa-check-circle"></i>Sukses!</strong>Data Ssiwa berhasil disimpan.
+ 					<strong><i class="fas fa-check-circle"></i>Sukses!</strong>Data Siswa berhasil disimpan.
  					<button type="button" class="close" data-dismiss="alert" aria-label="Close">
  						<span aria-hidden="true">&times;</span>
  					</button>
  				</div>
  				
  		<?php 
- 		?>
  		}
- 		
+ 		//jika alert = 2
+ 		//tampilkan pesan sukses "Data siswa berhasil diubah."
+ 		elseif ($_GET['alert'] == 2) { ?>
+ 		<div class="alert alert-success alert-dismissable fade show" role="alert">
+ 			<strong><i class="fas fa-check-circle"></i>Sukses!</strong>Data Siswa berhasil diubah.
+ 			<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+ 				<span aria-hidden="true">&times;</span>
+ 			</button>
+ 		</div>
+
+ 		<?php
+ 		}
+ 		//jika alert = 3
+ 		//tampilkan pesan sukses "Data siswa berhasil dihapus"
+ 		elseif ($_GET['alert'] == 3) { ?>
+ 			<div class="alert alert-success alert-dismissable fade show" role="alert"> 
+ 				<strong><i class="fas fa-check-circle"></i>Sukses!</strong>Data Siswa berhasil dihapus.
+ 				<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+ 					<span aria-hidden="true">&times;</span>
+ 				</button>
+ 			</div>
+
+ 		<?php
+ 		}
+ 		//jika alert = 4
+ 		//tampilkan pesan gagal "NIS sudah ada"
+ 		elseif ($_GET['alert'] == 4) { ?>
+
+ 		<div class="alert alert-success alert-dismissable fade show" role="alert">
+ 		<strong><i class="fas fa-check-circle"></i>Gagal!</strong> NIS <b><?php echo $_GET['nis']; ?></b> sudah ada.
+ 		<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+ 			<span aria-hidden="true">&times;</span>
+ 		</button>
+ 		</div>
+ 		<?php  
+ 	 	  }
+ 	 	?>
+
+ 	 	<form class="mr-3" action="index.php"  method="POST">
+ 	 		<div class="form-row">
+ 	 			<!-- form cari -->
+ 	 			<div class="col-3">
+ 	 				<input type="text" name="form-control" name="cari" placeholder="Cari NIS atau Nama Siswa" value="<?php echo $cari; ?>">
+ 	 			</div>
+ 	 			<!-- tombol cari -->
+ 	 			<div class="col-8">
+ 	 				<button type="submit" class="btn btn-info">Cari</button>
+ 	 			</div>
+ 	 		<!-- tombol tambah data -->
+ 	 			<div class="col">
+ 	 				<a class="btn btn-info" href="?page=tambah" role="button"><i class="fas fa-plus"></i>Tambah</a>
+ 	 			</div>
+ 	 		</div>
+ 	 	</form>
+
+ 	 	<!-- tabel untuk menampilkan data siswa -->
+ 	 	
  	</div>
  </div>
